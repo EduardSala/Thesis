25a72eb78b9acf6ea3b50227db0b793fc38f2625
Eduard Salahoru <esalahoru@gmail.com>
2026-02-03 14:09:03 +0100

    Add config, rename extraction module and stubs
    Add a new config/config.yaml with extraction and spatio-temporal matching settings. Rename modules/extract_insitu_data_to_csv.py -> modules/data_extraction_md.py and clean up the extraction function (remove unused imports, adjust return/print behavior and CSV write paths). Add a new stub modules/spatial_crossover_md.py with a spatial_cross signature. Update modules/.gitignore to ignore /test.py/ and simplify modules/test.py to list satellite CSV files and attempt a read (replace previous moorings NC-to-CSV conversion script).

--- modules/data_extraction_md.py
+++ modules/data_extraction_md.py
@@ -1,17 +1,7 @@
 import xarray as xr
 import pandas as pd
 import numpy as np
-from modules import Module_all_functions as md
 import os
-filepath_files = md.pass_filepath("../datasets/moorings-nc")
-
-# -----------------------------------------------------------------------------
-#variable_name = 'VAVH'
-#application = 'wave'
-
-variable_name = 'WSPD'
-application = 'wind'
-# -----------------------------------------------------------------------------
 
 def extr_insitu_data(filepath_file_nc,var_name,field,deph_val,tol=0.1):
     dataset_nc = xr.open_dataset(filepath_file_nc)
@@ -31,10 +21,10 @@ def extr_insitu_data(filepath_file_nc,var_name,field,deph_val,tol=0.1):
             dataframe_mooring = pd.DataFrame(
                 {'time': time, 'latitude': latitude, 'longitude': longitude,
                  variable_name: variable}).fillna(-1)
-            return [dataframe_mooring,mooring_name,field]
-            # dataframe_mooring.to_csv(r"../datasets/mooring-csv/" + mooring_name + "_wave" + ".csv")
-            dataframe_mooring.to_csv(folderDir_csv + f"/" + mooring_name + f"_{field}" + ".csv")
+            print("Extraction done!")
+            return dataframe_mooring,mooring_name,field
         else:
+            print("Extraction done!")
             return None
 
 
---
+++ modules/spatial_crossover_md.py
@@ -0,0 +1,1 @@
+def spatial_cross(filepath_sat_csv,filepath_mooring_csv,cross_radius,var):
