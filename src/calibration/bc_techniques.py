5bfbb3a4b87f84cbe04ebcd99a3d77c2d8c84625
Eduard <esalahoru@gmail.com>
2026-02-08 15:10:37 +0100

    Add docstrings and type hints for calibration

--- src/calibration/bc_techniques.py
+++ src/calibration/bc_techniques.py
@@ -3,7 +3,18 @@ import pandas as pd
 from calibration import calibration_methods as cal_mthd
 
 
-def linear_cal(df_sat: pd.DataFrame, df_mooring: pd.DataFrame, variable: str):
+def linear_cal(df_sat: pd.DataFrame, df_mooring: pd.DataFrame, variable: str) -> pd.DataFrame:
+    """
+    Calibrates a satellite variable using the first 10 days of reference mooring data, then applies the linear
+    transformation to the calibration dataset. The coefficients b and a are applied to the satellite validation dataset.
+    Args:
+        df_sat (pd.DataFrame): DataFrame containing satellite data.
+        df_mooring (pd.DataFrame): DataFrame containing mooring data.
+        variable (str): The name of the variable to calibrate.
+    Returns:
+        new_df_sat_validation (pd.DataFrame): A new DataFrame containing the calibrated satellite validation data for
+         the specified variable.
+    """
 
     df_mooring_cal, df_sat_cal, df_mooring_val, df_sat_val = cal_mthd.calib_df_first_ten_days(df_mooring, df_sat)
 
